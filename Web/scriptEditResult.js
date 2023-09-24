ef8a397ac71441d411e809aa94af20a12bfd403a
jona-terceros <jonatan.terceros@ucb.edu.bo>
2023-09-24 14:15:00 -0400

    fixing code smells reverting

--- Web/scriptEditResult.js
+++ Web/scriptEditResult.js
@@ -1,49 +1,49 @@
-document.addEventListener('DOMContentLoaded', async (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
 
 
     let  queryParams = window.location.search.split('&');
     let type = queryParams[0].split('=')[1];
     let gameId = queryParams[1].split('=')[1];
+ 
+
+    if(type == "edit"){
+        GetResultNormal();
+
+        document.getElementById('form-box').addEventListener('submit', UpdateGame);
+    }
+    else
+    {
+        GetGameToPlay();
+
+        document.getElementById('form-box').addEventListener('submit', UpdatePlayGame);
+    }
+        
     const baseRawUrl = 'http://localhost:5500';
 
-    const initializePage = () => {
-        if (type === "edit") {
-          GetResultNormal();
-          document.getElementById('form-box').addEventListener('submit', UpdateGame);
-        } else {
-          GetGameToPlay();
-          document.getElementById('form-box').addEventListener('submit', UpdatePlayGame);
-        }
-    };
+    async function GetResultNormal(event){
 
-    const handleMonthConversion = (month) => {
-        const monthMap = {
-          '01': 'Enero', '02': 'Febrero', '03': 'Marzo', '04': 'Abril', '05': 'Mayo',
-          '06': 'Junio', '07': 'Julio', '08': 'Agosto', '09': 'Septiembre',
-          '10': 'Octubre', '11': 'Noviembre', '12': 'Diciembre'
-        };
-        return monthMap[month] || month;
-      };
-    const fetchGameData = async () => {
         const url = `http://localhost:5500/api/game/${gameId}`;
-        const response = await fetch(url);
-        return await response.json();
-    };
-    const updateUI = (type, data) => {
-        const editForm = document.getElementById('form-box');
-        const localPath = `${baseRawUrl}/${data.localTeamPath}`;
-        const awayPath = `${baseRawUrl}/${data.awayTeamPath}`;
-        const localImage = `<img src="${localPath}"  width="180px" height="180px"></img>`;
-        const awayImage = `<img src="${awayPath}"  width="180px" height="180px"></img>`;
-        const title = `<p class="title-principal">${type === "edit" ? "Editar Partido" : "Simular Partido"}</p>`;
-    
-        document.getElementById('title-changer').innerHTML = title;
-        document.getElementById('localImage').innerHTML = localImage;
-        document.getElementById('awayImage').innerHTML = awayImage;
-    
-        populateForm(editForm, data);
-      };
-    const populateForm = (editForm, data) => {
+        let response = await fetch(url);
+
+        let data = await response.json();
+        let editForm = document.getElementById('form-box');
+
+        let month = data.month;
+        switch(month){
+            case '01':month= "Enero";break;
+            case '02':month= "Febrero";break;
+            case '03':month= "Marzo";break;
+            case '04':month= "Abril";break;
+            case '05':month= "Mayo";break;
+            case '06':month= "Junio";break;
+            case '07':month= "Julio";break;
+            case '08':month= "Agosto";break;
+            case '09':month= "Septiembre";break;
+            case '10':month= "Octubre";break;
+            case '11':month= "Noviembre";break;
+            case '12':month= "Diciembre";break;
+        }
+
         editForm.local.value = data.localTeam;
         editForm.visitante.value = data.awayTeam;
         editForm.stadium.value = data.stadium;
@@ -53,79 +53,173 @@ document.addEventListener('DOMContentLoaded', async (event) => {
         editForm[6].disabled = true;
         editForm[10].disabled = true;
         editForm.day.value = data.day;
-        editForm.month.value = handleMonthConversion(data.month);
+        editForm.month.value = month;
         editForm.dayWeek.value = data.dayWeek;
-        editForm.localGoals.value = data.localGoals;
-        editForm.awayGoals.value = data.awayGoals;
-    };
-    const disableFormFields = (editForm) => {
-        for (let i = 0; i < 14; i++) {
-          editForm[i].disabled = true;
+        editForm.localGoals.value = data.localGoals;//goles local
+        editForm.awayGoals.value = data.awayGoals;//goles visita
+
+
+        let localPath =  `${baseRawUrl}/${data.localTeamPath}`;
+        let awayPath = `${baseRawUrl}/${data.awayTeamPath}`;
+
+        let localImage = `<img src="${localPath}"  width="180px" height="180px"></img>`;
+        let awayImage = `<img src="${awayPath}"  width="180px" height="180px"></img>`;
+        let title = `<p class="title-principal">Editar Partido</p>`;
+
+        document.getElementById('title-changer').innerHTML=title;
+        document.getElementById('localImage').innerHTML=localImage;
+        document.getElementById('awayImage').innerHTML=awayImage;
+    }
+
+    async function GetGameToPlay(event){
+
+        const url = `http://localhost:5500/api/game/${gameId}`;
+        let response = await fetch(url);
+
+        let data = await response.json();
+        let editForm = document.getElementById('form-box');
+
+        let month = data.month;
+        switch(month){
+            case '01':month= "Enero";break;
+            case '02':month= "Febrero";break;
+            case '03':month= "Marzo";break;
+            case '04':month= "Abril";break;
+            case '05':month= "Mayo";break;
+            case '06':month= "Junio";break;
+            case '07':month= "Julio";break;
+            case '08':month= "Agosto";break;
+            case '09':month= "Septiembre";break;
+            case '10':month= "Octubre";break;
+            case '11':month= "Noviembre";break;
+            case '12':month= "Diciembre";break;
+        }
+
+        editForm.local.value = data.localTeam;
+        editForm.visitante.value = data.awayTeam;
+        editForm.stadium.value = data.stadium;
+        editForm.tournament.value = data.tournament;
+        editForm.stage.value = data.stageTournament;
+        editForm.matchday.value = data.matchdayTournament;
+        editForm.day.value = data.day;
+        editForm.month.value = month;
+        editForm.dayWeek.value = data.dayWeek;
+        editForm.localGoals.value = data.localGoals;//goles local
+        editForm.awayGoals.value = data.awayGoals;//goles visita
+
+        for(let i=0; i< 14; i++){
+            editForm[i].disabled = true;
         }
         editForm[8].disabled = false;
         editForm[9].disabled = false;
         editForm[13].disabled = false;
-      };
-    
-    const fetchAndUpdateGame = async (url) => {
-        try {
-          const response = await fetch(url, {
+
+        let localPath =  `${baseRawUrl}/${data.localTeamPath}`;
+        let awayPath = `${baseRawUrl}/${data.awayTeamPath}`;
+
+        let localImage = `<img src="${localPath}"  width="180px" height="180px"></img>`;
+        let awayImage = `<img src="${awayPath}"  width="180px" height="180px"></img>`;
+        let title = `<p class="title-principal">Simular Partido</p>`;
+
+        document.getElementById('title-changer').innerHTML=title;
+        document.getElementById('localImage').innerHTML=localImage;
+        document.getElementById('awayImage').innerHTML=awayImage;
+    }
+
+
+
+    function UpdatePlayGame(event){
+
+        console.log(event.currentTarget);
+        event.preventDefault();
+
+        
+        let localGoals=event.currentTarget.localGoals.value;
+        let awayGoals=event.currentTarget.awayGoals.value;
+
+        
+        let url = `http://localhost:5500/api/game?gameId=${gameId}&localGoals=${localGoals}&awayGoals=${awayGoals}`;
+        
+        fetch(url, {
             headers: { "Content-Type": "application/json; charset=utf-8" },
             method: 'PUT',
-          });
-    
-          if (response.status === 200) {
-            alert("Game updated successfully");
-            window.location.href = "result.html";
-          } else {
-            const errorData = await response.text();
-            debugger;
-            console.log(errorData);
-          }
-        } catch (error) {
-          debugger;
-          console.log(error);
+        }).then((response) => {
+            if (response.status === 200) {
+                alert("Game updated successfuly");
+                window.location.href = "result.html";
+            } 
+            else{
+                response.text().then((data) => {
+                    debugger;
+                    console.log(data);
+                });
+            }
+        }).catch((response) => {
+                debugger;
+                console.log(data);
+        });
+    }
+
+
+    function UpdateGame(event){
+        console.log(event.currentTarget);
+        event.preventDefault();
+
+        let month = "";
+
+        switch(event.currentTarget.month.value){
+            case "Enero":month="01";break;
+            case "Febrero":month="02";break;
+            case "Marzo":month="03";break;
+            case "Abril":month="04";break;
+            case "Mayo":month="05";break;
+            case "Junio":month="06";break;
+            case "Julio":month="07";break;
+            case "Agosto":month="08";break;
+            case "Septiembre":month="09";break;
+            case "Octubre":month="10";break;
+            case "Noviembre":month="11";break;
+            case "Diciembre":month="12";break;
         }
-      };
-      const GetResultNormal = async () => {
-        const data = await fetchGameData();
-        updateUI("edit", data);
-      };
-    
-      const GetGameToPlay = async () => {
-        const data = await fetchGameData();
-        const editForm = document.getElementById('form-box');
-        disableFormFields(editForm);
-        updateUI("play", data);
-      };
-    
-      const UpdatePlayGame = (event) => {
-        event.preventDefault();
-        const localGoals = event.currentTarget.localGoals.value;
-        const awayGoals = event.currentTarget.awayGoals.value;
-        const url = `http://localhost:5500/api/game?gameId=${gameId}&localGoals=${localGoals}&awayGoals=${awayGoals}`;
-        fetchAndUpdateGame(url);
-      };
-    
-      const UpdateGame = (event) => {
-        event.preventDefault();
-        const month = handleMonthConversion(event.currentTarget.month.value);
+
         const gameToUpdate = {
-          localTeam: event.currentTarget.local.value,
-          awayTeam: event.currentTarget.visitante.value,
-          stadium: event.currentTarget.stadium.value,
-          tournament: event.currentTarget.tournament.value,
-          stageTournament: event.currentTarget.stage.value,
-          matchdayTournament: event.currentTarget.matchday.value,
-          day: event.currentTarget.day.value,
-          month: month,
-          dayWeek: event.currentTarget.dayWeek.value,
-          localGoals: parseInt(event.currentTarget.localGoals.value),
-          awayGoals: parseInt(event.currentTarget.awayGoals.value),
-        };
-        const url = `http://localhost:5500/api/game/${gameId}`;
-        fetchAndUpdateGame(url,gameToUpdate);
-      };
-    
-      initializePage();
-    });
+            localTeam:      event.currentTarget.local.value,
+            awayTeam:       event.currentTarget.visitante.value,
+            stadium:        event.currentTarget.stadium.value,
+            tournament:     event.currentTarget.tournament.value,
+            stageTournament:event.currentTarget.stage.value,
+            matchdayTournament: event.currentTarget.matchday.value,
+            day:            event.currentTarget.day.value,
+            month:          month,
+            dayWeek:        event.currentTarget.dayWeek.value,
+            localGoals:     parseInt(event.currentTarget.localGoals.value),
+            awayGoals:      parseInt(event.currentTarget.awayGoals.value),
+        }
+
+        const gameJson = JSON.stringify(gameToUpdate);
+        let url = `http://localhost:5500/api/game/${gameId}`;
+        
+        fetch(url, {
+            headers: { "Content-Type": "application/json; charset=utf-8" },
+            method: 'PUT',
+            body: gameJson
+        }).then((response) => {
+            if (response.status === 200) {
+                alert("Game updated successfuly");
+                window.location.href = "result.html";
+            } 
+            else{
+                response.text().then((data) => {
+                    debugger;
+                    console.log(data);
+                });
+            }
+        }).catch((response) => {
+                debugger;
+                console.log(data);
+        });
+
+    }
+
+
+});
