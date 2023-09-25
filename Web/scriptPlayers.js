69a252d448a00a27ac4c917eef7997782daefa4f
Rene <ddcrene@gmail.com>
2023-09-24 20:54:34 -0400

    scriptPlayers.js: Code Smells - Refactoring condition

--- Web/scriptPlayers.js
+++ Web/scriptPlayers.js
@@ -112,8 +112,8 @@ window.addEventListener('DOMContentLoaded', function(event){
 
     function DeletePlayer(event){
         
-        let r = confirm("Are you sure you want to delete it?");
-        if (r == true) {
+        let result = confirm("Are you sure you want to delete it?");
+        if (result) {
             let playerId = this.dataset.deletePlayerId;
             let url = `http://localhost:5500/api/player/${playerId}`;
             fetch(url, { 
