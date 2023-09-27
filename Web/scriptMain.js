d35e41186a500924b749a8329ebc4dbf8dc402b0
Rene <ddcrene@gmail.com>
2023-09-26 20:02:50 -0400

    Solving main.js copy

--- Web/scriptMain.js
+++ Web/scriptMain.js
@@ -8,146 +8,165 @@ window.addEventListener('DOMContentLoaded', function(event){
     document.getElementById("footer").innerHTML = generateFooter();
 
     const baseRawUrl = 'http://localhost:5500';
-    async function GetResult(event){
-        const remGamesHtml = document.querySelectorAll('.month-game');
-        for (const rem of remGamesHtml) {
-            rem.remove();
-        }
-        let filterTournament = "";
-        if(this.dataset != undefined){
-            filterTournament = this.dataset.buttonFilter;
-        }
-        const url = `http://localhost:5500/api/game?finish=Terminado&tournament=${filterTournament}`;
+    async function GetResultAndNextGame(){
+        const url = `http://localhost:5500/api/game?twoGamesOnly=true`;
         let response = await fetch(url);
-        let errorText;
         try{
             if(response.status == 200){
                 let data = await response.json();
-                const day = data.map(g => `<div class="date-day"><p>${g.day}</p></div>`);
-                const dayWeek = data.map(g => `<div class="date-day-literal"><p>${g.dayWeek}</p></div>`);
-                const month = data.map(g => `<div class="date-month"><p>${g.month}</p></div>`);
-                const monthText = data.map(g => g.month);
-                const localImage = data.map(g => g.localTeamPath? `${baseRawUrl}/${g.localTeamPath}` : "");
-                const awayImage = data.map(g => g.awayTeamPath? `${baseRawUrl}/${g.awayTeamPath}` : "");
-                const stage = data.map(g => `<div class="stage"><p>${g.stageTournament}</p></div>`);
-                const matchday = data.map(g => `<div class="matchday"><p>${g.matchdayTournament}</p></div>`);
-                const localTeam = data.map(g => `<div class="team-name"><p>${g.localTeam}</p></div>`);
-                const goals = data.map(g => `<div class="local-goals"><p>${g.localGoals}</p></div><div class="versus"><p>-</p></div><div class="away-goals"><p>${g.awayGoals}</p></div>`);
-                const awayTeam = data.map(g => `<div class="team-name"><p>${g.awayTeam}</p></div>`);
-                const stadium = data.map(g => `<div class="stadium-name"><p>${g.stadium}</p></div>`);
-                const tournament = data.map(g => g.tournament);
-                const gameId = data.map(g => g.id);
-                const listMonths = [];
-                let newMonth = "";
-                let i;
-                for(i = 0; i < month.length; i++){
-                    if(newMonth != monthText[i]){
-                        newMonth = monthText[i];
-                        listMonths.push(newMonth);
-                    }
-                }
-                let gamesByMonth = "";
-                let fullContent = "";
-                let counter =0;
-                for(i = 0; i < listMonths.length; i++){
-                    while(counter < monthText.length && listMonths[i] == monthText[counter]){
-                        let contentGame=`<div class="game-buttons" title~="${i}">
-                                            <div class="game">
-                                                <div class="game-header">
-                                                ${day[counter]}
-                                                <div class="date-day-month">
-                                                    ${dayWeek[counter]}
-                                                    <div class="date-month"><p>${convertTextMonth(monthText[counter])}</p></div>
-                                                </div>
-                                                </div>
-                                                <div class="tournament">
-                                                    <div class="tournament-image"><img src="${tournamentPathImage(tournament[counter])}" alt="Liga" width="40px" height="40px"></div>
-                                                    ${stage[counter]}${matchday[counter]}
-                                                </div>
-                                                <div class="teams">
-                                                    <div class="local-team">
-                                                        <div class="local-team-image"><img src="${localImage[counter]}" alt="Plamaflor" width="70px" height="70px"></div>
-                                                        ${localTeam[counter]}
-                                                    </div>
-                                                    <div class="result-hour">${goals[counter]}</div>
-                                                    <div class="away-team">
-                                                        <div class="away-team-image"><img src="${awayImage[counter]}" alt="Wilsterman" width="70px" height="70px"></div>
-                                                        ${awayTeam[counter]}
-                                                    </div>
-                                                </div>
-                                                <div class="stadium">
-                                                    <div class="stadium-image"><img src="./Images/Otros/stadium plantilla.png" alt="Stadium" width="30px" height="30px"></div>
-                                                    ${stadium[counter]}
-                                                </div>
+
+                let day = data.map(g => `<div class="date-day"><p>${g.day}</p></div>`);
+                let dayWeek = data.map(g => `<div class="date-day-literal"><p>${g.dayWeek}</p></div>`);
+                let localImage = data.map(g => g.localTeamPath? `${baseRawUrl}/${g.localTeamPath}` : "");
+                let awayImage = data.map(g => g.awayTeamPath? `${baseRawUrl}/${g.awayTeamPath}` : "");
+                let stage = data.map(g => `<div class="stage"><p>${g.stageTournament}</p></div>`);
+                let matchday = data.map(g => `<div class="matchday"><p>${g.matchdayTournament}</p></div>`);
+                let localTeam = data.map(g => `<div class="team-name"><p>${g.localTeam}</p></div>`);
+                let goals = data.map(g => `<div class="local-goals"><p>${g.localGoals}</p></div><div class="versus"><p>-</p></div><div class="away-goals"><p>${g.awayGoals}</p></div>`);
+                let awayTeam = data.map(g => `<div class="team-name"><p>${g.awayTeam}</p></div>`);
+                let stadium = data.map(g => `<div class="stadium-name"><p>${g.stadium}</p></div>`);
+                let timeMatch = data.map(g => `<div class="hour"><p>${g.hour}</p></div><div class="two-points"><p>:</p></div><div class="minute"><p>${g.minutes}</p></div>`);
+                let tournament = data.map(g => g.tournament);
+                let monthText = data.map(g => g.month);
+
+
+                let contentResult = `<div class="result-header">
+                                        ${day[0]}
+                                        <div class="date-day-month">
+                                            ${dayWeek[0]}
+                                            <div class="date-month"><p>${convertTextMonth(monthText[0])}</p></div>
+                                        </div>
+                                    </div>
+                                    <div class="tournament">
+                                        <div class="tournament-image"><img src="${tournamentPathImage(tournament[0])}" alt="Liga" width="40px" height="40px"></div>
+                                        ${stage[0]}${matchday[0]}
+                                    </div>
+                                    <div class="teams">
+                                        <div class="local-team">
+                                            <div class="local-team-image"><img src="${localImage[0]}" alt="Plamaflor" width="70px" height="70px"></div>
+                                            ${localTeam[0]}
+                                        </div>
+                                        <div class="result-hour">${goals[0]}</div>
+                                        <div class="away-team">
+                                            <div class="away-team-image"><img src="${awayImage[0]}" alt="Wilsterman" width="70px" height="70px"></div>
+                                            ${awayTeam[0]}
+                                        </div>
+                                    </div>
+                                    <div class="stadium">
+                                        <div class="stadium-image"><img src="./Images/Otros/stadium plantilla.png" alt="Stadium" width="30px" height="30px"></div>
+                                        ${stadium[0]}
+                                    </div>`;
+                
+
+                if(day.length > 1)
+                {
+
+                    let contentGame =   `<div class="result-header">
+                                            ${day[1]}
+                                            <div class="date-day-month">
+                                                ${dayWeek[1]}
+                                                <div class="date-month"><p>${convertTextMonth(monthText[1])}</p></div>
                                             </div>
-                                            <div class="buttons-game">
-                                                <button class="edit" data-edit-game-id="${gameId[counter]}">
-                                                    <img src="./Images/Icons/CREATE.gif" alt="">
-                                                    <p class="text-button">EDIT GAME</p>
-                                                </button>
-                                                <button class="delete" data-delete-game-id="${gameId[counter]}">
-                                                    <img src="./Images/Icons/DELETE.gif" alt="">
-                                                    <p class="text-button">DELETE GAME</p>
-                                                </button>
+                                        </div>
+                                        <div class="tournament">
+                                            <div class="tournament-image"><img src="${tournamentPathImage(tournament[1])}" alt="Liga" width="40px" height="40px"></div>
+                                            ${stage[1]}${matchday[1]}
+                                        </div>
+                                        <div class="teams">
+                                            <div class="local-team">
+                                                <div class="local-team-image"><img src="${localImage[1]}" alt="Plamaflor" width="70px" height="70px"></div>
+                                                ${localTeam[1]}
                                             </div>
-                                        </div>`;
-                        gamesByMonth = gamesByMonth+contentGame;
-                        counter=counter+1;
-                    }
-                    let contentMonth = `<div class="month-game">
-                                            <div class="month-bigger"><p>${convertTextMonth(listMonths[i])}</p></div>
-                                            <div class="list-games">
-                                                ${gamesByMonth}
+                                            <div class="result-hour">${timeMatch[1]}</div>
+                                            <div class="away-team">
+                                                <div class="away-team-image"><img src="${awayImage[1]}" alt="Wilsterman" width="70px" height="70px"></div>
+                                                ${awayTeam[1]}
                                             </div>
+                                        </div>
+                                        <div class="stadium">
+                                            <div class="stadium-image"><img src="./Images/Otros/stadium plantilla.png" alt="Stadium" width="30px" height="30px"></div>
+                                            ${stadium[1]}
                                         </div>`
-                    fullContent = fullContent+contentMonth;
-                    gamesByMonth = "";
+                    document.getElementById('game').innerHTML = contentGame;
                 }
-                document.querySelector('.filters').insertAdjacentHTML("afterend", fullContent);
-                let deleteButtons = document.querySelectorAll('[data-delete-game-id]'); //Delete
-                for (const button of deleteButtons) {
-                    button.addEventListener('click', DeleteGame);
-                }
-                let editButtons = document.querySelectorAll('[data-edit-game-id]'); //Edit
-                for (const button of editButtons) {
-                    button.addEventListener('click', goToEditGame);
-                }
+
+                document.getElementById('result').innerHTML = contentResult;
+                
                 
             } else {
-                errorText = await response.text();
+                let errorText = await response.text();
                 alert(errorText);
             }
         } catch(error){
-            errorText = await error.text();
+            let errorText = await error.text();
             alert(errorText);
         }
     }
-    function DeleteGame(event){
-        
-        let r = confirm("Are you sure you want to delete it?");
-        if (r) {
-            let gameId = this.dataset.deleteGameId;
-            let url = `http://localhost:5500/api/game/${gameId}`;
-            fetch(url, { 
-            method: 'DELETE' 
-            }).then((data)=>{
-                if(data.status === 200){
-                    alert('deleted');
-                }
-            }); 
-            location.reload();
-        } 
-    }
-    function goToEditGame(event){
-        let gameId = this.dataset.editGameId;
-        window.location.href = `editResults.html?type=edit&gameId=${gameId}`;
-    }
-    
 
-    //Disparadores
-    GetResult();
-    let filterButtons = document.querySelectorAll('[data-button-filter]'); //Filtros
-    for (const button of filterButtons) {
-        button.addEventListener('click', GetResult);
+
+
+
+
+
+    async function GetRumors(){
+        const url = `http://localhost:5500/api/rumor`;
+        let response = await fetch(url);
+        try{
+            if(response.status == 200){
+                let data = await response.json();
+
+                let type = data.map(r => `<p class="type-transfer-literal">${r.type}</p>`);
+                let currency = data.map(r => `<div class="currency"><p>${r.currency}</p></div>`);
+                let price = data.map(r => `<div class="price"><p>${r.price}</p></div>`);
+                let playerImage = data.map(g => g.playerPath? `${baseRawUrl}/${g.playerPath}` : "");
+                let teamImage = data.map(g => g.targetTeamPath? `${baseRawUrl}/${g.targetTeamPath}` : "");
+                let targetTeam = data.map(r => `<div class="target-team-name"><p>${r.targetTeam}</p></div>`);
+                let playerName = data.map(r => `<div class="player-transfer-name"><p>${r.playerName}</p></div>`);
+                let transfer = data.map(r => r.transfer);
+                let inOutImage;
+                let fullContent = "";
+
+                for(let i = 0; i < 3; i++){
+                    switch(transfer[i]){
+                        case "Salida":{inOutImage = "./Images/Otros/salida.gif";break}
+                        case "Llegada":{inOutImage = "./Images/Otros/llegada.gif";break}
+                    }
+                    let transfersContent=   `<div class="transfer">
+                                                <div class="transfer-header">
+                                                    ${price[i]}
+                                                    ${currency[i]}
+                                                </div>
+                                                <div class="type-transfer">
+                                                    <div class="type-transfer-image"><img src="./Images/Otros/transfer-color.gif" alt="Transfer" width="40px" height="40px"></div>
+                                                    ${type[i]}
+                                                </div>
+                                                <div class="actors-transfer">
+                                                    <div class="player-transfer">
+                                                        <div class="player-transfer-image"><img src="${playerImage[i]}" width="90px" height="90px"></div>
+                                                        ${playerName[i]}
+                                                    </div>
+                                                    <div class="transfer-in-out"><img src="${inOutImage}" width="74px" height="74px"></div>
+                                                    <div class="transfer-target-team">
+                                                        <div class="target-team-image"><img src="${teamImage[i]}" width="90px" height="90px"></div>
+                                                        ${targetTeam[i]}
+                                                    </div>
+                                                </div>
+                                            </div>`
+
+                    fullContent = fullContent+transfersContent;
+                }
+                document.querySelector('.more-rumors').insertAdjacentHTML("afterend", fullContent);
+
+            } else {
+                let errorText = await response.text();
+                alert(errorText);
+            }
+        } catch(error){
+            let errorText = await error.text();
+            alert(errorText);
+        }
     }
+
+    GetResultAndNextGame();
+    GetRumors();
 });
--- Web/scriptResults.js
+++ Web/scriptResults.js
@@ -1,5 +1,6 @@
 import generateFooter from "./Components/footerComponent.js";
 import generateMenu from "./Components/menuComponent.js";
+import {convertTextMonth, tournamentPathImage} from "./Repository/extraFunctions.js";
 
 window.addEventListener('DOMContentLoaded', function(event){
 
@@ -159,38 +160,6 @@ window.addEventListener('DOMContentLoaded', function(event){
         let gameId = this.dataset.editGameId;
         window.location.href = `editResults.html?type=edit&gameId=${gameId}`;
     }
-
-    
-
-    function convertTextMonth(month){
-        switch(month){
-            case '01':return 'Enero';
-            case '02':return 'Febrero';
-            case '03':return 'Marzo';
-            case '04':return 'Abril';
-            case '05':return 'Mayo';
-            case '06':return 'Junio';
-            case '07':return 'Julio';
-            case '08':return 'Agosto';
-            case '09':return 'Septiembre';
-            case '10':return 'Octubre';
-            case '11':return 'Noviembre';
-            case '12':return 'Diciembre';
-        }
-    }
-
-    function tournamentPathImage(tournament){
-        switch(tournament){
-            case "Copa Sudamericana":{return "./Images/Tournaments/Copa Sudamericana.png";}
-            case "Liga Boliviana":{return "./Images/Tournaments/Liga Boliviana.png";}
-            case "Amistoso":{return "./Images/Tournaments/amistoso.png";}
-            default:{return "./Images/Tournaments/Liga Boliviana.png";}
-        }
-    }
-
-
-
-
 
     //Disparadores
 
